fada325813717f490ed20a5e66cefe652b42a8f0
lulujordanna <lulu@brainsights.ca>
2019-09-07 16:09:01 -0400

    Week02 project files

---
+++ week02/week02.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+var content = fs.readFileSync('/home/ec2-user/environment/week01/data/m10.txt');
+var $ = cheerio.load(content);
+
+fs.writeFileSync('/home/ec2-user/environment/week02/data/m10.txt', location);
+var location; 
+
+$('tr tr tr').each(function(i, elem) {
+    
+    if (i != 0){
+        
+        //Remove the extra elements (div, spans and bolded text) that are in the table data
+        var meetingTitle = $(elem).find('b').remove().html();
+        var extraDiv = $(elem).find('div').remove().html(); 
+        var extraSpan = $(elem).find('span').remove().html(); 
+    } 
+        //Finds the need text in the table data and removes the white space
+        var address = $(elem).children().eq(0).text().replace(/\t/g,'').split('\n').filter(address=>address.trim().length > 1).map(i=>i.trim()); 
+        
+        //Extracts the key information of the address. 
+        location = address[1] + ' Manhattan NY '; 
+        console.log(location);
+        
+        //Appending the information onto the file itself without overriding the previous information. 
+        fs.appendFileSync('/home/ec2-user/environment/week02/data/m10.txt', location + '\n' );
+});
