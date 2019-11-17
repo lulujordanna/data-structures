9f59911183c4880a6c8115ed6b2013ebf4123f5f
EC2 Default User <ec2-user@ip-172-31-87-108.ec2.internal>
2019-11-17 20:43:59 +0000

    WIP week10 app

--- week10/app.js
+++ week10/app.js
@@ -1,33 +1,44 @@
-var express = require('express'), // npm install express
-    app = express();
-
+//Dependenices 
 const dotenv = require('dotenv');
 dotenv.config({path: '/home/ec2-user/environment/.env'});
 const { Client } = require('pg');
+var async = require('async');
+var express = require('express'),
+    app = express();
 
-var aaoutput = [];
-
-//////////////////////////////////
-app.get('/', function(req, res) {
-    res.send(`<h1>Data Structures Apps</h1>
-            <ul><li><a href="/aa.html">AA App.</a></li></ul>
-            <ul><li><a href="/process.html">Process Blog App.</a></li></ul>
-            <ul><li><a href="/sensor.html">Sensor App.</a></li></ul>`);
-});
-
-////////////////////////////////
-app.get('/aa.html', function(req, res) {
-    var db_credentials = new Object();
+//SQL dependencies
+ var db_credentials = new Object();
     db_credentials.user = process.env.AWSRDS_USER;
     db_credentials.host = process.env.AWSRDS_HOST;
     db_credentials.database = 'aa';
     db_credentials.password = process.env.AWSRDS_PW;
     db_credentials.port = 5432;
 
-  const client = new Client(db_credentials);
-  client.connect();
+    const client = new Client(db_credentials);
+    client.connect();
 
-    var thisQuery = `WITH locationWithZone as (
+//DyanmoDB dependencies
+var AWS = require('aws-sdk');
+AWS.config = new AWS.Config();
+AWS.config.region = "us-east-1";
+
+//////////////////////////////////
+app.get('/', function(req, res) {
+    res.send(`<h1>Data Structures Final Assignments</h1>
+            <ul><li><a href="/aa.html">Alcoholics Anonymous: Meeting Finder</a></li></ul>
+            <ul><li><a href="/process.html">Learning JS: A Process Blog</a></li></ul>
+            <ul><li><a href="/sensor.html">Cool November: A Temperature Visualization</a></li></ul>`);
+});
+
+////////////////////////////////
+var aaOutput = []
+
+app.get('/aa.html', function(req, res) {
+    res.send(aaOutput);
+});
+
+//SQL Query for AA meetings
+    var firstQuery = `WITH locationWithZone as (
                 SELECT *, SPLIT_PART(l.locationid,'_',1) as zoneID
                 FROM locationGeo l
                 )
@@ -35,38 +46,68 @@ app.get('/aa.html', function(req, res) {
                  SELECT l.*, z.*
                  FROM locationWithZone l
                  INNER JOIN zoneNames z 
-                    on l.zoneID  = z.zoneID
-                 WHERE z.zoneName = 'Lower East Side/Soho';`;
+                    on l.zoneID  = z.zoneID;`;
 
-    client.query(thisQuery, (err, response) => {
-        var output = response.rows;
-        aaoutput.push(output);
-        console.log(err);
-        var rawHtml = '';
-        for (var i=0; i<aaoutput[0].length; i++){
-            rawHtml += "<p>" + JSON.stringify(aaoutput[0][i]) + "</p>"
-        }
-        res.send(rawHtml);
-        client.end();
-    });
+client.query(firstQuery, (err, res) => {
+    if (err) {throw err}
+    else {
+        aaOutput.push(res.rows);
+        // console.log(aaOutput);
+        // client.end();
+    }
 });
-
 //////////////////////////////////
-
-app.get('/process.html', function(req, res) {
-    res.send('<h3>this is the page for my sensor data</h3>');
-});
-
-//////////////////////////////////
+var sensorOutput = [];
 
 app.get('/sensor.html', function(req, res) {
-    res.send('<h3>this is the page for my sensor data</h3>');
+    res.send(sensorOutput);
 });
 
+//SQL query for sensor data: 
+var thirdQuery = "SELECT sensorValue, COUNT (*) FROM sensorData GROUP BY sensorValue;"; // print the number of rows for each sensorValue
+    
+    client.query(thirdQuery, (err, res) => {
+        if (err) {throw err}
+        else {
+            // console.table(res.rows);
+            sensorOutput.push(res.rows);
+        }
+         client.end();
+    });
 //////////////////////////////////
 
-// serve static files in /public
-app.use(express.static('public'));
+
+app.get('/process.html', function(req, res) {
+    var processOutput = [];
+    var dynamodb = new AWS.DynamoDB();
+    
+    var params = {
+        TableName : "processblog",
+        KeyConditionExpression: "#tp = :categoryName and #dt between :minDate and :maxDate", // the query expression
+        ExpressionAttributeNames: { // name substitution, used for reserved words in DynamoDB
+            "#tp" : "category", 
+            "#dt" : "date"
+        },
+        ExpressionAttributeValues: { // the query values
+            ":categoryName": {S: "AA Meetings"}, 
+            ":minDate": {S: new Date("August 30, 2019").toDateString()},
+            ":maxDate": {S: new Date("December 11, 2019").toDateString()}
+        }
+    };
+    
+    dynamodb.query(params, function(err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Query succeeded.");
+            data.Items.forEach(function(item) {
+            processOutput.push(item)
+            });
+         res.send(processOutput);
+        }
+    });
+});
+//////////////////////////////////
 
 // listen on port 8080
 app.listen(8080, function() {
